25b6d0ffd3412aa0528936d5bf5de2172d525a3d
“Sebastian” <s.garcia@iimoney.co>
2021-06-10 23:10:24 -0500

    update manifest in docs

---
+++ docs/precache-manifest.7907971b9318c51bab341aaf7c41d9df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce5d1dcb76e9e2ecc2af",
+    "url": "/me/static/css/main.dbc4bf49.chunk.css"
+  },
+  {
+    "revision": "ce5d1dcb76e9e2ecc2af",
+    "url": "/me/static/js/main.ce5d1dcb.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "10b17337e30c7972410cf4eed5a9de25",
+    "url": "/me/index.html"
+  }
+];

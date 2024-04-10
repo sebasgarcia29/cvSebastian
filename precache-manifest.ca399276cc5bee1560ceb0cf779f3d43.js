2a88ab29a43b7b3a27813b200e4cd5a3198047a2
sebastian <sebasuchiha29.sg@gmail.com>
2024-04-09 19:44:26 -0500

    Updates

---
+++ precache-manifest.ca399276cc5bee1560ceb0cf779f3d43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd1e954a52ef34b71849",
+    "url": "/me/static/css/main.410647fc.chunk.css"
+  },
+  {
+    "revision": "cd1e954a52ef34b71849",
+    "url": "/me/static/js/main.cd1e954a.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "634d2c417b53c1005378d66d176a0789",
+    "url": "/me/index.html"
+  }
+];
